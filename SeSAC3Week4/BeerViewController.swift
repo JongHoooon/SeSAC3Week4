caedc842d372d41809f1e3c5dfb99fafb0d8c0eb
JongHoon <qnm541@gmail.com>
2023-08-08 14:18:55 +0900

    beer image 받아오는 로직 수정

--- SeSAC3Week4/BeerViewController.swift
+++ SeSAC3Week4/BeerViewController.swift
@@ -41,12 +41,24 @@ final class BeerViewController: UIViewController {
                 let imageURL = json["image_url"].stringValue
                 let description = json["description"].stringValue
                 
+                /*
                 DispatchQueue.global().async {
                     let beerImage = try? UIImage(data: Data(contentsOf: URL(string: imageURL)!))
                     DispatchQueue.main.async {
                         self?.beerImageView.image = beerImage
                     }
                 }
+                 */
+                
+                Task {
+                    do {
+                        self?.beerImageView.image = try await self?.fetchImage(url: imageURL)
+                    } catch {
+                        print(error)
+                    }
+                }
+                
+             
                 
                 self?.nameLabel?.text = name
                 self?.descriptionLabel?.text = description
@@ -54,5 +66,22 @@ final class BeerViewController: UIViewController {
                 print(error)
             }
         }
+    }
+    
+    private func fetchImage(url: String) async throws -> UIImage {
+        
+        print(Thread.current)
+        
+        guard let url = URL(string: url) else {
+            throw fatalError("잘못된 이미지 url입니다.")
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        guard let image = UIImage(data: data) else {
+            throw fatalError("유효하지 않은 data 입니다.")
+        }
+        
+        return image
     }
 }
